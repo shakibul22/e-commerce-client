05d1d012ab3ec60833c080ce71c1028d266aa4d1
Kamranislam0123 <kamranislam8@gmail.com>
2024-01-25 18:51:54 +0600

    cart added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import Testimonials from '../../Components/Testimonials'
-import Discount from '../../Components/Discount'
-import TopCategory from '../../Components/TopCategory'
-import NewArrivals from '../../Components/NewArrivals'
-import Banner from '../../Components/Banner'
+import React from "react";
+import Testimonials from "../../Components/Testimonials";
+import Discount from "../../Components/Discount";
+import TopCategory from "../../Components/TopCategory";
+import NewArrivals from "../../Components/NewArrivals";
+import Banner from "../../Components/Banner";
+import Cart from "../Cart";
 
 function Home() {
   return (
     <div>
-        <Banner/>
-        <TopCategory/>
-        <NewArrivals/>
-        <Discount/>
-        <Testimonials/>
-
-
+      <Banner />
+      <TopCategory />
+      <NewArrivals />
+      <Discount />
+      <Testimonials />
+      <Cart />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
