5813a5d56827c2c85768f66ec212160159e5186f
shakibul22 <shakibulislam684@gmail.com>
2024-01-25 18:52:17 +0600

    testimonials added

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -1,86 +1,55 @@
-import React from "react";
-// import Slider from "react-animated-slider";
-// import "react-animated-slider/build/horizontal.css";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-
-// import required modules
-import { Pagination } from "swiper/modules";
-
-const content = [
-  {
-    title: "Testimonial 1",
-    description: "Description for Testimonial 1.",
-    button: "Read More",
-    image: "url-to-image-1.jpg",
-  },
-  {
-    title: "Testimonial 2",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex veniam, nisi voluptatem aliquid qui odio illo reiciendis tenetur ipsam officia neque, est ducimus non doloremque adipisci amet dolores nobis!",
-    button: "Read More",
-    image: "url-to-image-2.jpg",
-  },
-  {
-    title: "Testimonial 2",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex veniam, nisi voluptatem aliquid qui odio illo reiciendis tenetur ipsam officia neque, est ducimus non doloremque adipisci amet dolores nobis!",
-    button: "Read More",
-    image: "url-to-image-2.jpg",
-  },
-  {
-    title: "Testimonial 2",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex veniam, nisi voluptatem aliquid qui odio illo reiciendis tenetur ipsam officia neque, est ducimus non doloremque adipisci amet dolores nobis!",
-    button: "Read More",
-    image: "url-to-image-2.jpg",
-  },
-
-  {
-    title: "Testimonial 2",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex veniam, nisi voluptatem aliquid qui odio illo reiciendis tenetur ipsam officia neque, est ducimus non doloremque adipisci amet dolores nobis!",
-    button: "Read More",
-    image: "url-to-image-2.jpg",
-  },
-  // Add more testimonial items as needed
-];
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Autoplay, Pagination } from 'swiper/modules';
 
 function Testimonials() {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  };
+
   return (
-    <section >
-     
-
-        <Swiper
-        
-          pagination={{
-            dynamicBullets: true,
-            
-            clickable: true,
-            horizontalClass:'swiper-pagination-vertical'
-          }}
-          modules={[Pagination]}
-        >
-          {content.map((item, index) => (
-            <SwiperSlide
-              key={index}
-              style={{
-                background: `url('${item.image}') no-repeat center center`,
-              }}
-            >
-              <div className="center">
-                <h1>{item.title}</h1>
-                <p>{item.description}</p>
-                <div className="flex flex-col">
-
-                <button >{item.button}</button>
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-
-    </section>
+    <div className='px-[520px]'>
+       <h1 className='text-4xl text-center py-9 pt-16'>Testmonials</h1>
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper"
+        autoplay={{ delay: 3000 }} // Autoplay with a 3-second delay
+      >
+        <SwiperSlide>
+          <div>
+            <img
+              src="https://res.cloudinary.com/dzqxgr4lr/image/upload/v1706180246/bg-1_hvp1xl.jpg"
+              alt=""
+              style={{ width: "100%", height: "45vh", objectFit: "cover" }}
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <img
+              src="https://res.cloudinary.com/dzqxgr4lr/image/upload/v1706180402/bg-2_1_gi8bdg.jpg"
+              alt=""
+              style={{ width: "100%", height: "45vh", objectFit: "cover" }}
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <img
+              src="https://res.cloudinary.com/dzqxgr4lr/image/upload/v1706180334/bg-2_qxlblp.jpg"
+              alt=""
+              style={{ width: "100%", height: "45vh", objectFit: "cover" }}
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 }
 
