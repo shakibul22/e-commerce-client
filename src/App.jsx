42a1f3af60a8a1b1a04618e79278a36ead3676e4
Kamranislam0123 <kamranislam8@gmail.com>
2024-01-25 16:51:01 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { Outlet } from "react-router-dom"
-import Header from "./Components/Header"
-
+import { Outlet } from "react-router-dom";
+import Header from "./Components/Header";
+import { Footer } from "./Components/Footer";
 
 function App() {
-
   return (
     <div id="wrapper">
-     <Header/>
-     <Outlet/>
-    
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
