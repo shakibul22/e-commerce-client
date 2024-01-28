5c4fc462195dc3cc7e02410fa9850bbcfad44c16
Kamranislam0123 <kamranislam8@gmail.com>
2024-01-28 15:39:58 +0600

    banner text modified

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -11,11 +11,11 @@ function Banner() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true, // Enable auto-play
-    autoplaySpeed: 3000, // Set the auto-play speed in milliseconds (e.g., 3000ms = 3 seconds)
-  
+    autoplay: true,
+    autoplaySpeed: 3000,
     appendDots: (dots) => (
-      <div className="rounded-full"
+      <div
+        className="rounded-full"
         style={{
           borderRadius: "10px",
           padding: "10px",
@@ -24,22 +24,21 @@ function Banner() {
         <ul style={{ margin: "0px" }}> {dots} </ul>
       </div>
     ),
-    // Inside the settings object:
     customPaging: (i) => (
-      <div className="text-[10vh] transform -translate-y-24 absolute left-5 "
+      <div
+        className="text-[10vh] transform -translate-y-24 absolute left-5 "
         style={{
           width: "30px",
-          height:"40px",
+          height: "40px",
           color: "white",
           position: "absolute",
-          // Adjust the spacing between dots
           display: "flex",
           justifyContent: "start",
-          alignItems: "center", // Align vertically
-          zIndex: 10, // Set the z-index
+          alignItems: "center",
+          zIndex: 10,
         }}
       >
-       .
+        .
       </div>
     ),
   };
@@ -48,25 +47,76 @@ function Banner() {
     <div className="">
       <Slider {...settings}>
         <div>
-          <img
-            src="https://res.cloudinary.com/dzqxgr4lr/image/upload/v1706180246/bg-1_hvp1xl.jpg"
-            alt=""
-            style={{ width: "100%", height: "45vh", objectFit: "cover" }}
-          />
+          {/* Image 1 with text */}
+          <div style={{ position: "relative", width: "100%", height: "45vh" }}>
+            <img
+              src="https://res.cloudinary.com/dzqxgr4lr/image/upload/v1706180246/bg-1_hvp1xl.jpg"
+              alt=""
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "20%",
+                transform: "translate(-50%, -50%)",
+                color: "white",
+                textAlign: "center",
+              }}
+            >
+              <h1>Your Text Here</h1>
+              <p>Additional description if needed</p>
+            </div>
+          </div>
         </div>
         <div>
-          <img
-            src="https://res.cloudinary.com/dzqxgr4lr/image/upload/v1706180402/bg-2_1_gi8bdg.jpg"
-            alt=""
-            style={{ width: "100%", height: "45vh", objectFit: "cover" }}
-          />
+          {/* Image 2 with text */}
+          <div style={{ position: "relative", width: "100%", height: "45vh" }}>
+            <img
+              src="https://res.cloudinary.com/dzqxgr4lr/image/upload/v1706180402/bg-2_1_gi8bdg.jpg"
+              alt=""
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "20%",
+                transform: "translate(-50%, -50%)",
+                color: "white",
+                textAlign: "left",
+              }}
+            >
+              <h1>Your Text Here</h1>
+              <p>Additional description if needed</p>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Button
+              </button>
+            </div>
+          </div>
         </div>
         <div>
-          <img
-            src="https://res.cloudinary.com/dzqxgr4lr/image/upload/v1706180334/bg-2_qxlblp.jpg"
-            alt=""
-            style={{ width: "100%", height: "45vh", objectFit: "cover" }}
-          />
+          {/* Image 3 with text */}
+          <div style={{ position: "relative", width: "100%", height: "45vh" }}>
+            <img
+              src="https://res.cloudinary.com/dzqxgr4lr/image/upload/v1706180334/bg-2_qxlblp.jpg"
+              alt=""
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "20%",
+                transform: "translate(-50%, -50%)",
+                color: "white",
+                textAlign: "center",
+              }}
+            >
+              <h1>Your Text Here</h1>
+              <p>Additional description if needed</p>
+            </div>
+          </div>
         </div>
       </Slider>
     </div>
