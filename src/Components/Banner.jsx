b0d17a16502471c2b0fc33c1786327e895d28f65
shakibul22 <shakibulislam684@gmail.com>
2024-01-29 16:11:39 +0600

    aos added

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "aos/dist/aos.css";
+import AOS from "aos";
 
 function Banner() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    });
+  }, []);
+
   const settings = {
     dots: true,
     fade: true,
@@ -11,11 +19,11 @@ function Banner() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true, // Enable auto-play
-    autoplaySpeed: 3000, // Set the auto-play speed in milliseconds (e.g., 3000ms = 3 seconds)
-  
+    autoplay: true,
+    autoplaySpeed: 2000,
     appendDots: (dots) => (
-      <div className="rounded-full"
+      <div
+        className="rounded-full"
         style={{
           borderRadius: "10px",
           padding: "10px",
@@ -24,48 +32,71 @@ function Banner() {
         <ul style={{ margin: "0px" }}> {dots} </ul>
       </div>
     ),
-    // Inside the settings object:
     customPaging: (i) => (
-      <div className="text-[10vh] transform -translate-y-24 absolute left-5 "
+      <div
+        className="text-[10vh] transform -translate-y-24 absolute left-5"
         style={{
           width: "30px",
-          height:"40px",
+          height: "40px",
           color: "white",
           position: "absolute",
-          // Adjust the spacing between dots
           display: "flex",
           justifyContent: "start",
-          alignItems: "center", // Align vertically
-          zIndex: 10, // Set the z-index
+          alignItems: "center",
+          zIndex: 10,
         }}
       >
-       .
+        .
       </div>
     ),
   };
 
   return (
-    <div className="">
+    <div className="min-h-[75vh]">
       <Slider {...settings}>
         <div>
+          <div
+            className="text-start left-16 lg:left-48 flex flex-col justify-between gap-2 lg:gap-5 absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            data-aos="fade-right"
+          >
+            <p>Only today we offer free shopping</p>
+            <h1 className="  text-xl lg:text-7xl font-extrabold">Summer Collection</h1>
+            <button className="btn btn-outline btn-info w-[50%]">Check-out</button>
+          </div>
           <img
             src="https://res.cloudinary.com/dzqxgr4lr/image/upload/v1706180246/bg-1_hvp1xl.jpg"
             alt=""
-            style={{ width: "100%", height: "45vh", objectFit: "cover" }}
+            style={{ width: "100%", height: "75vh", objectFit: "cover" }}
           />
         </div>
         <div>
+        <div
+            className="text-start left-16 lg:left-48 flex flex-col justify-between gap-2 lg:gap-5 absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            data-aos="fade-right"
+          >
+              <p>Only today we offer free shopping</p>
+            <h1 className="text-white text-xl lg:text-7xl font-extrabold">Women Collection</h1>
+            <button className="btn btn-outline btn-info w-[50%]">Check-out</button>
+          </div>
           <img
             src="https://res.cloudinary.com/dzqxgr4lr/image/upload/v1706180402/bg-2_1_gi8bdg.jpg"
             alt=""
-            style={{ width: "100%", height: "45vh", objectFit: "cover" }}
+            style={{ width: "100%", height: "75vh", objectFit: "cover" }}
           />
         </div>
         <div>
+        <div
+            className="text-start left-16 lg:left-48 flex flex-col justify-between gap-2 lg:gap-5 absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            data-aos="fade-right"
+          >
+              <p>Only today we offer free shopping</p>
+            <h1 className="text-white text-xl lg:text-7xl font-extrabold">Fashion Collection</h1>
+            <button className="btn btn-outline btn-success w-[50%]">Shop Now</button>
+          </div>
           <img
             src="https://res.cloudinary.com/dzqxgr4lr/image/upload/v1706180334/bg-2_qxlblp.jpg"
             alt=""
-            style={{ width: "100%", height: "45vh", objectFit: "cover" }}
+            style={{ width: "100%", height: "75vh", objectFit: "cover" }}
           />
         </div>
       </Slider>
