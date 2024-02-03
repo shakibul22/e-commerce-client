4d3d0a6bccb255980d43075327306a85057e1c80
Kamranislam0123 <kamranislam8@gmail.com>
2024-02-03 21:37:21 +0600

    popup component added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,14 +4,23 @@ import Discount from "../../Components/Discount";
 import TopCategory from "../../Components/TopCategory";
 import NewArrivals from "../../Components/NewArrivals";
 import Banner from "../../Components/Banner";
+import BestSellingProduct from "../../Components/BestSellingProduct";
+import SayHello from "../../Components/SayHello";
+import PopupModal from "../../Components/PopupModal";
 
 function Home() {
+
   return (
     <div>
+      <PopupModal />
       <Banner />
       <TopCategory />
       <NewArrivals />
+      <BestSellingProduct />
       <Discount />
+      <SayHello  />
+    
+
       <Testimonials />
     </div>
   );
